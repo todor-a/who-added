28f2ce08e4acb10a8eda8ba0253a9bac63697879
Todor Andonov <todor.andonov@payhawk.com>
2025-04-24 22:17:21 +0300

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+import { execSync } from 'node:child_process';
+import { parseArgs } from 'node:util';
+
+const { values, positionals } = parseArgs({
+    options: {
+        dependency: { type: 'string', short: 'd' },
+        file: { type: 'string', short: 'f', default: 'package.json' },
+    },
+    strict: false,
+    allowPositionals: true,
+});
+
+const dep = values.dependency || positionals[0];
+const file = values.file;
+
+if (!dep) {
+    console.error('Error: dependency name is required.');
+    console.error('Usage: who-added --dependency react [--file package.json]');
+    process.exit(1);
+}
+
+const gitCmd = [
+    'git',
+    'log',
+    '--reverse',
+    `-S'"${dep}\":'`,
+    "--pretty=format:'%H %an %ad %s'",
+    '--',
+    file,
+].join(' ');
+
+try {
+    const output = execSync(gitCmd, {
+        encoding: 'utf8',
+        stdio: ['ignore', 'pipe', 'inherit'],
+    });
+    const firstLine = output.split('\n')[0];
+    if (!firstLine) {
+        console.log(
+            `No commits found adding or removing '"${dep}\":' in ${file}.`,
+        );
+    } else {
+        console.log('First commit introducing dependency:');
+        console.log(firstLine);
+    }
+} catch (err) {
+    console.error('Error running git command:', err);
+    process.exit(1);
+}
